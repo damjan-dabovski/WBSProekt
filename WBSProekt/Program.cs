5852442c7ffc2f022fba622bcbeb8ff78838ac81
Damjan Dabovski <damjan.dabovski@students.finki.ukim.mk>
2020-08-01 18:54:12 +0200

    Initial commit

---
+++ WBSProekt/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace WBSProekt {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
